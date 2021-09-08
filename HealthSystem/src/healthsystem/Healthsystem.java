80f47583b6a9d7ffc4e34212f05fff82c0eb9ec9
Julio Mollo <73155080+ProfeJulio@users.noreply.github.com>
2021-09-08 17:30:17 +0000

    Agregué la estructura del proyecto y un código de prueba usando un script bash para ejecutar

--- HealthSystem/src/healthsystem/Healthsystem.java
+++ HealthSystem/src/healthsystem/Healthsystem.java
@@ -1,5 +1,7 @@
+package healthsystem;
+
 class Healthsystem {
   public static void main(String[] args) {
-    System.out.println("Adios curso");
+    System.out.println("Hola Adios curso");
   }
 }
