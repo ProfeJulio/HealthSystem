144b54245325c9a38a4602cfd84564570794903b
ProfeJulio <jmollom@cldvarica.cl>
2022-03-23 15:30:07 -0300

    Prueba de modificación en editaro Atom desde el plugins Githab

--- HealthSystem/src/healthsystem/Healthsystem.java
+++ HealthSystem/src/healthsystem/Healthsystem.java
@@ -1,7 +1,7 @@
 package healthsystem;
 
 class Healthsystem {
-  
+
   public static void main(String[] args) {
     Jugador Jprincipal = new Jugador("Heroe", 15);
     Zombie zombito = new Zombie("Zombito", 500);
@@ -12,11 +12,11 @@ class Healthsystem {
     Jprincipal.recibirVida(MiBotiquin.curarVida());
     System.out.println(">> La vida del Jugador es "+Jprincipal.getVida()+"\n");
 
-  
+    System.out.println("\n\nAQUI ESTOY!!!\n\n")
 
     Jprincipal.recibirDanio( zombito.ataque() );
     System.out.println(">> El Zombie ataca a Jugador Principal. Jugador Principal tiene "+Jprincipal.getVida()+" de vida");
-   
+
     zombito.recibirDanio( Jprincipal.ataque() );
     System.out.println(">> El Jugador Principal ataca a zombito. Zombito tiene "+zombito.getVida()+" de vida");
 
